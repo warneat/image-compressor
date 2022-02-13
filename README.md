b277c079878626c6313cb2cc9eb2f15ff265db78
warneat <41758828+warneat@users.noreply.github.com>
2022-02-14 00:01:21 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,64 @@
+## Image-Compressor
+
+(tl,dr:) compress your images taken with an Android-Phone into subfolders sorted by years with reduced filesize.<br />
+
+If you run out of storage on your smartphone and don't want to spend hours deleting and sorting out pictures, this is an easy solution.<br />
+
+Reduce the file-size of your images on the backup device (NAS?) and shove them back to your phone.
+
+### Notes:
+- The Android Naming-Convention IMG_YYYYMMDD_HHmmSS.jpg is assumed.
+- The scipt only deals with .jpg files.
+- By default, file size will be reduced to about 25% of the original size, while maintaining acceptable quality.
+- No files will be deleted!
+- .jpg files which do not fit the pattern (e.g. have weird numbers after extension, as i experienced) are copied without changes.
+- files without '.jpg' are being ignored.
+- It does not run through subdirectories.
+
+Simply place `image_compressor.py` in the directory, where your pictures are sitting and run it. 
+
+### Installation
+Assuming Python and Pip3 is installed, in terminal:
+
+clone this repository, cd into it:
+
+        git clone https://github.com/warneat/image-compressor && cd image-compressor
+
+Install dependencies:
+
+        pip3 -r install requirements.txt
+
+move the script to your image folder e.g: 
+    
+        mv image_compressor.py ~/foo/bar/images
+
+clean up with
+
+        cd .. && rm -r image-compressor
+
+###Run
+
+In your image directory run the script with 
+    
+        python3 image_compressor.py
+    or
+        ./image_compressor.py
+
+
+
+
+
+### Further reading
+
+- By default, compressing value is set to `quality=50`, Antialiasing is enabled. To modify settings:
+  - change `quality=xx`to your liking (line 55) and/or: 
+  - remove `, Image.ANTIALIAS` in line 54 to disable antialising (i don't see a reason to do so...)
+  
+- The script runs on 4 processes (Raspberry Pi: All 4 cores 100%) :)
+
+- As the processing might take hours or days for large amounts of data, when acessing via remote/ssh you might want to use screen ([ultra-quick tutorial](https://linuxize.com/post/how-to-use-linux-screen/)) to keep it running without an open terminal session.<br> 
+In a nutshell: Install it, `$screen` to start, [do stuff], `ctr+a` and then `d` to detach. Reattach with `$screen -r`
+
+- To automate (image) backups e.g Phone to NAS/Cloud-Server/Online-Account... i highly recommend the app ([FolderSync](https://play.google.com/store/apps/details?id=dk.tacit.android.foldersync.lite)) 
+
+#### Feedback is very much apprechiated
---
+++ image_compressor.py
@@ -0,0 +1,200 @@
+#!/usr/bin/env python3
+import os
+import inspect
+import time
+import shutil
+from PIL import Image
+from multiprocessing import Process
+
+
+def chunk_list(lst, n):
+    lengh = len(lst)
+    chunk_size = int(lengh/n)
+
+    if lengh % n ==0:
+        for i in range(0,lengh, chunk_size):
+            yield lst[i:i + chunk_size]
+    else:
+        chunk_size = int(lengh/n) +1 #awful but good enough
+        for i in range(0, lengh, chunk_size):
+            yield lst[i:i + chunk_size]
+
+def compressing_loop(compressed_dir, script_dir, chunked_list):
+
+    # which process is doing what??
+    #print(f"Process ID: {os.getpid()}. I'll do {len(chunked_list)} images...")
+
+    for jpg in chunked_list:
+
+        #files and directory infos
+        year = jpg_year(jpg)
+        new_dir = os.path.join(compressed_dir, str('IMG_'+year))
+        old_path = os.path.join(script_dir, jpg)
+        new_path = str(os.path.join(new_dir, jpg)).replace('.jpg', '_compressed.jpg')
+        
+        
+        try:
+            #new dir available?
+            if os.path.exists(new_dir) is False:
+                os.mkdir(new_dir)
+            #image already compressed?
+            elif os.path.exists(new_path):
+                pass
+            #happened once, not sure why... try ignoring
+        except FileExistsError:
+            continue
+
+        #load, process, save
+        with Image.open(fp=old_path) as image:
+
+            # resize
+            newsize = int(image.height/2), int(image.width/2)  # has no effect...?!
+
+            # save with new name, new size and lower quality
+            image.resize(newsize, Image.ANTIALIAS)
+            image.save(new_path, quality=50)
+            image.close()
+
+            # examples:
+            # test set: 83 MB 
+            # quality=85 reduces to: 36.4 MB 43%
+            # quality=50 reduces to: 16.9 MB 23%
+
+def copying_loop(compressed_dir, script_dir, copy_targets):
+    for copy_target in copy_targets:
+
+        #files and directory infos
+        year = jpg_year(copy_target)
+        new_dir = os.path.join(compressed_dir, str('IMG_'+year))
+        old_path = os.path.join(script_dir, copy_target)
+        new_path = str(os.path.join(new_dir, copy_target)).replace('.jpg', '_compressed.jpg')
+        
+        try:
+            # new dir available?
+            if os.path.exists(new_dir) is False:
+                os.mkdir(new_dir)
+            #image already copied?
+            elif os.path.exists(new_path):
+                pass
+        #happened once, not sure why... try ignoring    
+        except FileExistsError:
+            continue
+
+        # copy file
+        shutil.copyfile(src=old_path, dst=new_path)
+        #print(f'Copied without compressing: {copy_target}')
+
+def jpg_targets_lists(script_dir):
+    '''list of valid .jpg file names including extension'''
+    all_filenames = os.listdir(script_dir)
+
+    target_jpg_list = []
+    copy_list = []
+    for filename in all_filenames:
+        if (filename.split('.')[-1] == 'jpg' and                  # Extension
+            filename[0] != '.' and                  # not hidden
+            filename[:4] == 'IMG_'):                # convension _
+                target_jpg_list.append(filename)
+        elif ('.jpg' in filename and
+            filename[0] != '.' and
+            filename[:4] == 'IMG_'):
+                copy_list.append(filename)
+    return target_jpg_list, copy_list
+
+def jpg_year(jpg):
+    # assuming Android naming convention DCIM_2019 :(
+    year = jpg.split('_')[1][:4]
+    return year
+
+def count_files_in_dirs(output_dir):
+    #recursive
+    file_amount = 0 
+    for dirpath, dirnames, filenames in os.walk(output_dir):
+        for f in filenames:
+            fp = os.path.join(dirpath, f)
+            if not os.path.islink(fp):
+                file_amount +=1
+    return file_amount
+
+def progress_bar(compressed_dir, total, barLength = 20):
+    #prints out a nice progress bar by checking number of files
+    count = True
+    while count is True:
+        current = count_files_in_dirs(compressed_dir)
+
+        # print actual bar:
+        percent = float(current) * 100 / total
+        arrow   = '-' * int(percent/100 * barLength - 1) + '>'
+        spaces  = ' ' * (barLength - len(arrow))
+        print('Progress: [%s%s] %d %%' % (arrow, spaces, percent), end='\r')
+        if percent >= 100:
+            count = False
+            print('Progress: [%s%s] %d %%' % (arrow, spaces, percent))
+        else:
+            time.sleep(1)
+
+def main():
+    try:
+        # where am i? Path infos
+        scrip_path = inspect.getframeinfo(inspect.currentframe()).filename
+        script_dir = os.path.dirname(os.path.abspath(scrip_path))
+        compressed_dir = script_dir+'/IMG_compressed'
+
+        #find targets
+        compress_jpg_targets, copy_jpg_targets = jpg_targets_lists(script_dir)
+        jpg_compress_amout = len(compress_jpg_targets)
+        jpg_copy_amount = len(copy_jpg_targets)
+
+        # 0 targets ->stop
+        if not jpg_compress_amout:
+            print('No files to compress in this directory')
+        else:
+            print(f'\nFound {jpg_compress_amout} valid .jpg files to compress')
+            print(f'Found {jpg_copy_amount} .jpg files to copy without compressing\n')
+        
+            #chunk targets for 4 processes
+            chunk_1, chunk_2, chunk_3, chunk_4 = chunk_list(compress_jpg_targets, 4)
+
+            # .../compressed folder available?
+            try:
+                if os.path.exists(compressed_dir) is False:
+                    os.mkdir(compressed_dir)
+            except FileExistsError:
+                pass
+
+            #copy strange .jpg files without compressing
+            copying_loop(compressed_dir,script_dir, copy_jpg_targets)
+
+            #go!
+            p1 = Process(target=compressing_loop, args=(compressed_dir, script_dir, chunk_1))
+            p2 = Process(target=compressing_loop, args=(compressed_dir, script_dir, chunk_2))
+            p3 = Process(target= compressing_loop, args=(compressed_dir, script_dir, chunk_3))
+            p4 = Process(target= compressing_loop, args=(compressed_dir, script_dir, chunk_4))
+
+            process_list = [p1, p2, p3, p4]
+
+            #start processes
+            for p in process_list:
+                p.start()
+
+            #verbotity while running
+            progress_bar(compressed_dir, (jpg_compress_amout+jpg_copy_amount), barLength=40)
+            print()
+
+            #close processes
+            for p in process_list:
+                p.join()
+            
+            for p in process_list:
+                p.close
+
+    except KeyboardInterrupt:
+        #close processes
+        for p in process_list:
+            p.join()
+        
+        for p in process_list:
+            p.close
+        print('bye')
+if __name__ == '__main__':
+    main()
